64e956ab773a89a85e48a31eeed728d10ab9d0fa
shapexd <newobj95@gmail.com>
2020-03-11 20:35:30 +0900

    nodejs 10강까지

---
+++ nodejs/main.js
@@ -0,0 +1,21 @@
+var http = require('http');
+var fs = require('fs');
+var url = require('url');
+
+var app = http.createServer(function(request,response){
+    var _url = request.url;
+    var queryData = url.parse(_url, true).query; // 요청된 url의 쿼리데이터 가져오기
+    console.log(queryData.id);
+    if(_url == '/'){
+      _url = '/index.html';
+    }
+    if(_url == '/favicon.ico'){
+      response.writeHead(404);
+      response.end();
+      return;
+    }
+    response.writeHead(200);
+    response.end(queryData.id);
+
+});
+app.listen(3000);
