1acafb9d400e437db986591ad35e91082c8c9f31
shapexd <newobj95@gmail.com>
2020-03-14 13:06:17 +0900

    뭐지모름

--- nodejs/exam/readdir.js
+++ nodejs/exam/readdir.js
@@ -1,8 +1,8 @@
-const testFolder = '../data';
-const fs = require('fs');
+const testFolder = "../data";
+const fs = require("fs");
 
 fs.readdir(testFolder, (err, files) => {
-  if(err) throw err;
+  if (err) throw err;
   files.forEach(file => {
     console.log(file);
   });
